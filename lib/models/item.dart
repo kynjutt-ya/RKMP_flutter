22ba7223aa3587fb54cc68d4b2770764a30c41a9
Julia Kugut <kugut.yulya@mail.ru>
2025-10-03 20:33:30 +0300

    Модель данных по объявлениям

---
+++ lib/models/item.dart
@@ -0,0 +1,15 @@
+class Item {
+  final String id;
+  final String title;
+  final String description;
+  final bool forExchange;
+  final String owner;
+
+  Item({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.forExchange,
+    required this.owner,
+  });
+}
